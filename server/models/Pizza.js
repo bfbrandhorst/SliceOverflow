d6e970e688d4303168de64983f2707d542d3c426
Chris Babb <Chris Babb>
2024-07-23 19:10:56 -0500

    pizza model added

--- server/models/Pizza.js
+++ server/models/Pizza.js
@@ -1,1 +1,31 @@
 // name, image, description(ingredients), price, size, allergens contained
+
+const { Schema, model } = require('mongoose');
+
+const pizzaSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    categories: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Category',
+        }
+    ]
+});
+
+const Pizza = model('Pizza', pizzaSchema)
+
+module.exports = Pizza;
