c5d5c016f0948dca64cc14c402013bde4aac8599
bfbrandhorst <bfbrandhorst@gmail.com>
2024-07-25 17:47:53 -0600

    Home.jsx and Home.CSS created, changes to all 3 package.json, App.jsx created

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import '../css/Home.css'
+import HomePic2 from '../assets/HomePic2.jpg'
+
+const Home =()=>{
+    return (
+        <>
+            <section className='homeBanner'>
+                <div className='Heading'>
+                <h1 >Slice Overflow</h1>
+                <ul className= 'Nav'>
+                    <li>Home</li>
+                    <li>Menu</li>
+                    <li>Login or Signup</li>
+                    <li>My Account</li>
+                    <li>Checkout</li>
+                </ul>
+                </div>
+                
+            </section>
+
+            <section className='MockOrder'>
+                <div className='location'>
+                    <h2>Albuquerque, NM</h2>
+                    <p>500 Artisan Lane</p>
+                    <p>HOURS: 11:00 AM - 10:00 PM</p>
+                    <button className='OrderNow'>Order Online</button>
+                </div>
+                <div className='location'>
+                    <h2>Dallas, TX</h2>
+                    <p>800 Ryan Street</p>
+                    <p>SUN - THURS: 11:00 AM- 9:00 PM </p>
+                    <p>FRI & SAT: 11:00 AM - 11:00 PM</p>
+                    <button className='OrderNow'>Order Online</button>
+                </div>
+            </section>
+        
+        </>
+    )
+}
+
+
+export default Home
