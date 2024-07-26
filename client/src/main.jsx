43beb67c99f63c8e3a5552eb26d409a3ddcb8c32
bfbrandhorst <bfbrandhorst@gmail.com>
2024-07-25 19:01:04 -0600

    Added NavBar.jsx, added code to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,37 +1,38 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
-import './css/index.css'
+import Home from './pages/Home.jsx'
+import Menu from './pages/Menu.jsx'
+import LoginSignup from './pages/LoginSignup.jsx' 
+import CartCheckout from './pages/CartCheckout.jsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Home/>
+      }, {
+        path: '/Menu',
+        element: 'Menu'
+      }, {
+        path: '/LoginSignup',
+        element: 'Login or Signup'
+      }, {
+        path: '/CartCheckout',
+        element: 'Checkout'
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
-
-
-// import ReactDOM from 'react-dom/client'
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import App from './App.jsx'
-// import SearchBooks from './pages/SearchBooks'
-// import SavedBooks from './pages/SavedBooks'
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//     errorElement: <h1 className="display-2">Wrong page!</h1>,
-//     children: [
-//       {
-//         index: true,
-//         element: <SearchBooks />
-//       }, {
-//         path: '/saved',
-//         element: <SavedBooks />
-//       }
-//     ]
-//   }
-// ])
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <RouterProvider router={router} />
-// )
