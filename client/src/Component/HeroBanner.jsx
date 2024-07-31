95a17a5449464c1ad3042577e94080be33c99ccf
George <ghern5234@gmail.com>
2024-07-31 13:40:02 -0500

    edited hero and homepage

--- client/src/Component/HeroBanner.jsx
+++ client/src/Component/HeroBanner.jsx
@@ -1,6 +1,11 @@
-export function HeroBanner() {
+export function HeroBanner({children}) {
     return (
-        <div className="h-96 bg-[url('assets/HomePic2.jpg')] bg-center bg-cover">
+        <>
+        
+        <div className="h-screen bg-[url('assets/HomePic2.jpg')] bg-center bg-cover">
+        <div className="h-full absolute w-full"></div>
+        <div className="flex flex-row items-around justify-around h-full w-full">{children}</div>
         </div>
+        </>
     )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
 import '../css/Home.css'
-import HomePic2 from '../../public/assets/HomePic2.jpg'
 import {HeroBanner} from '../Component/HeroBanner'
 
 const Home =()=>{
     return (
-        <>
-    <HeroBanner />
-            <section className='MockOrder'>
-                <div className='location'>
+        <div className=''>
+    <HeroBanner>
+    <section className='flex flex-row justify-around items-center w-full'>
+                <div className='bg-gray-300 rounded-xl p-4 px-9 m-4'>
                     <h2>Albuquerque, NM</h2>
                     <p>500 Artisan Lane</p>
-                    <p>HOURS: 11:00 AM - 10:00 PM</p>
-                    <button className='OrderNow rounded-full bg-black text-white p-4 mt-4'>Order Online</button>
+                    <p>SUN - THURS: 11:00 AM- 9:00 PM </p>
+                    <p>FRI & SAT: 11:00 AM - 11:00 PM</p>
+                    <button className='rounded-full bg-black text-white p-4 mt-4'>Order Online</button>
                 </div>
-                <div className='location'>
+                <div className='bg-gray-300 rounded-xl p-4 px-9 m-4'>
                     <h2>Dallas, TX</h2>
                     <p>800 Ryan Street</p>
                     <p>SUN - THURS: 11:00 AM- 9:00 PM </p>
                     <p>FRI & SAT: 11:00 AM - 11:00 PM</p>
-                    <button className='OrderNow'>Order Online</button>
+                    <button className='rounded-full bg-black text-white p-4 mt-4'>Order Online</button>
                 </div>
             </section>
+    </HeroBanner>
         
-        </>
+        </div>
     )
 }
 
