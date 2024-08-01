82672ef2f9279dc332f66a961ded8d81ea442815
Chris Babb <Chris Babb>
2024-08-01 10:30:10 -0500

    updated checkout to include total, not display items with quantity of zero

--- client/src/Component/CartCheckoutForm.jsx
+++ client/src/Component/CartCheckoutForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { handleCheckout } from "../pages/Cart/handleCheckout";
-
+import { useNavigate } from "react-router-dom";
 import {
   Container,
   Typography,
@@ -22,6 +22,18 @@ const CartCheckoutForm = () => {
   const [showAlert, setShowAlert] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
+  const filteredCart = cart.filter(item => item.quantity > 0);
+
+  const navigate = useNavigate();
+
+  const handleCheckout = () => {navigate("/checkoutResult")}
+
+  const totalPrice = filteredCart.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
+
+  const totalWithTax = totalPrice * 1.0825
+
   return (
     <Container maxWidth="sm">
       <Box>
@@ -34,8 +46,7 @@ const CartCheckoutForm = () => {
           </Alert>
         )}
         <List>
-
-          {cart.map((item, index) => (
+          {filteredCart.map((item, index) => (
             <div key={item._id}>
               <ListItem>
                 <ListItemText
@@ -47,6 +58,11 @@ const CartCheckoutForm = () => {
             </div>
           ))}
         </List>
+
+        <Typography variant="h6" component="p" align="right">
+          Total: ${totalPrice.toFixed(2)}<br></br>
+          Total with Tax: ${totalWithTax.toFixed(2)}
+        </Typography>
 
         <Box mt={2}>
           <Button
